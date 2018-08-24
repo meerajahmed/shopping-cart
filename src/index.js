7701999745d7a0dba6a8a3e403ffb8cc4fe538c1
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-24 10:09:37 +0530

    react counter app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
+import Counter from './containers/counter';
+import store from './store'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
-const App = () => <h1>React Seed App</h1>;
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <Provider store={store}>
+    <Counter />
+  </Provider>,
+  document.getElementById('root'));
