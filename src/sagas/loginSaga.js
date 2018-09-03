c84992c924714380123f265969c0c2fe75eaf353
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-03 22:24:16 +0530

    manage app login side effects using saga

--- src/sagas/loginSaga.js
+++ src/sagas/loginSaga.js
@@ -1,19 +1,18 @@
 import { take, put, call, actionChannel } from 'redux-saga/effects'
 
-import signUpAPI from "../api/signUpAPI";
-import setCurrentUserInfo from "../actions/setCurrentUserInfo";
-import {setAuthToken, SIGN_UP} from "../actions/auth";
+import {LOGIN, setAuthToken} from "../actions/auth";
+import loginAPI from "../api/loginAPI";
+import {setUserInfo} from "../actions/user";
 
-export default function* currentUserSaga() {
-  const signUpChannel = yield actionChannel(SIGN_UP);
+export default function* loginSaga() {
+  const loginChannel = yield actionChannel(LOGIN);
   while (true) {
     try{
-      const {payload: {user}} = yield take(signUpChannel);
-      console.log(user);
-      const {data, headers} = yield call(signUpAPI, user);
+      const {payload: {user}} = yield take(loginChannel);
+      const {data, headers} = yield call(loginAPI, user);
       yield put(setAuthToken(headers['x-auth']));
-      sessionStorage.setItem('authToken',headers['x-auth']);
-      yield put(setCurrentUserInfo(data));
+      sessionStorage.setItem('authToken', headers['x-auth']);
+      yield put(setUserInfo(data));
     } catch(error) {
       console.log(error);
     }
