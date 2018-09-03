86f63112eb6b8237775848375fe0f79fc304ce41
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-03 22:23:42 +0530

    manage app books side effects using saga

--- src/sagas/booksSaga.js
+++ src/sagas/booksSaga.js
@@ -1,25 +1,19 @@
 import { take, put, call, actionChannel } from 'redux-saga/effects'
 
-import getUserInfoAPI from "../api/getUserInfoAPI";
-import {setAuthToken} from "../actions/auth";
-import {GET_USER_INFO, setUserInfo} from "../actions/user";
-import {GET_BOOKS} from "../actions/books";
+import booksAPI from "../api/booksAPI";
+
+import {GET_BOOKS, setBooks} from "../actions/books";
 
 export default function* booksSaga() {
   const booksChannel = yield actionChannel(GET_BOOKS);
   while (true) {
     yield take(booksChannel);
     try{
-      const authToken = sessionStorage.getItem('authToken');
-      if(authToken) {
-        const data = yield call(getUserInfoAPI, authToken);
-        yield put(setAuthToken(authToken));
-        yield put(setUserInfo(data));
-      }
+      const books = yield call(booksAPI);
+      yield put(setBooks(books));
     } catch(error) {
       console.log(error);
     }
-
   }
 }
 
