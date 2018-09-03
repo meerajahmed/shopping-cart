8230484adbb1703c13b6691e381a72c0dd0cf2fe
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-03 22:25:23 +0530

    manage app search filter side effects using saga

--- src/sagas/searchSaga.js
+++ src/sagas/searchSaga.js
@@ -1,3 +1,12 @@
-/**
- * Created by meeahmed on 9/3/2018.
- */
+import {delay} from 'redux-saga'
+import {call, put, takeLatest} from 'redux-saga/effects'
+import {CATEGORY_CHANGE, SEARCH_INPUT_CHANGE, setSearchCategory, setSearchText} from "../actions/search";
+
+function* handleSearchChange({payload: {searchText}}) {
+  yield call(delay, 1000); // debounce by 1s
+  yield put(setSearchText(searchText));
+}
+
+export default function* watchInput() {
+  yield takeLatest(SEARCH_INPUT_CHANGE, handleSearchChange);
+}
