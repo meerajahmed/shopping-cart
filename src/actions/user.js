992dfaf7facae233ea4c4cafd3fd2576fea66757
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-04 22:28:40 +0530

    user action creator

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,3 +1,16 @@
-/**
- * Created by meeahmed on 9/2/2018.
- */
+
+export const SET_USER_INFO = 'SET_USER_INFO';
+export const setUserInfo = (data) => ({
+  type: SET_USER_INFO,
+  payload: {...data}
+});
+
+export const GET_USER_INFO = "GET_USER_INFO";
+export const getUserInfo = () => ({
+  type: GET_USER_INFO
+});
+
+export const REMOVE_USER_INFO = 'REMOVE_USER_INFO';
+export const removeUserInfo = () => ({
+  type: REMOVE_USER_INFO
+});
