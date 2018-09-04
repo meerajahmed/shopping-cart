8029cfc162439efac16f1b910408b658baddaa51
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-04 22:28:10 +0530

    cart action creator

--- src/actions/cart.js
+++ src/actions/cart.js
@@ -1,10 +1,49 @@
-export const GET_BOOKS = 'GET_BOOKS';
-export const getBooks = () => ({
-  type: GET_BOOKS
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const addToCart = (book, quantity = 1) => ({
+  type: ADD_TO_CART,
+  payload: {
+    book,
+    quantity
+  }
 });
 
-export const SET_BOOKS = 'SET_BOOKS';
-export const setBooks = (books) => ({
-  type: SET_BOOKS,
-  payload: {books}
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const removeFromCart = (cartId) => ({
+  type: REMOVE_FROM_CART,
+  payload: {cartId}
+});
+
+export const REMOVE_CART_ITEM = 'REMOVE_CART_ITEM';
+export const removeCartItem = (cartId) => ({
+  type: REMOVE_CART_ITEM,
+  payload: {cartId}
+});
+
+export const ADD_CART_ITEM = 'ADD_CART_ITEM';
+export const addCartItem = (cartItem) => ({
+  type: ADD_CART_ITEM,
+  payload: {cartItem}
+});
+
+export const ADD_CART_ITEMS = 'ADD_CART_ITEMS';
+export const addCartItems = (cartItems) => ({
+  type: ADD_CART_ITEMS,
+  payload: {cartItems}
+});
+
+export const REMOVE_CART_ITEMS = 'REMOVE_CART_ITEMS';
+export const removeCartItems = () => ({
+  type: REMOVE_CART_ITEMS
+});
+
+export const UPDATE_CART_ITEM = 'UPDATE_CART_ITEM';
+export const updateCartItem = (cartId, quantity) => ({
+  type: UPDATE_CART_ITEM,
+  payload: {cartId, quantity}
+});
+
+export const UPDATE_CART = 'UPDATE_CART';
+export const updateCart = (cartId, quantity) => ({
+  type: UPDATE_CART,
+  payload: {cartId, quantity}
 });
