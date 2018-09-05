96e7f8f5f870d0b034a1ad6cedaa2b017088b6b2
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-05 22:29:52 +0530

    login api

--- src/api/loginAPI.js
+++ src/api/loginAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default (user) =>
-  axios.post('http://localhost:3000/users',
+  axios.post('http://localhost:3000/users/login',
     {...user},
     {headers: {'Content-Type': 'application/json'}})
     .then((response) => response)
