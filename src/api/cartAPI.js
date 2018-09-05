2395cc2426317834552fa8373cc1e587c25c71d2
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-05 22:29:40 +0530

    cart api

--- src/api/cartAPI.js
+++ src/api/cartAPI.js
@@ -1,12 +1,39 @@
 import axios from 'axios';
 
-export default (authToken) =>
-  axios.get('http://localhost:3000/users/me', {
-    headers: {
-      'Content-Type': 'application/json',
-      'x-auth': authToken
-    }
-  }).then(response => response.data)
+export const addToCartAPI = ({bookId, quantity, authToken}) =>
+  axios.post('http://localhost:3000/carts',
+    {bookId, quantity},
+    {headers: {'Content-Type': 'application/json', 'x-auth': authToken}})
+    .then(response => response.data)
     .catch(err => {
       throw err;
     });
+
+export const fetchCartAPI = (authToken) =>
+  axios.get('http://localhost:3000/carts',
+    {headers: {'Content-Type': 'application/json', 'x-auth': authToken}})
+    .then(response => response.data)
+    .catch(err => {
+      throw err;
+    });
+
+
+export const updateCartAPI = ({cartId, quantity, authToken}) =>
+  axios.put(`http://localhost:3000/carts/${cartId}`,
+    {quantity},
+    {headers: {'Content-Type': 'application/json', 'x-auth': authToken}})
+    .then(response => response.data)
+    .catch(err => {
+      throw err;
+    });
+
+export const removeFromCartAPI = ({cartId, authToken}) =>
+  axios.delete(`http://localhost:3000/carts/${cartId}`,
+    {headers: {'Content-Type': 'application/json', 'x-auth': authToken}})
+    .then(response => response.data)
+    .catch(err => {
+      throw err;
+    });
+
+
+
