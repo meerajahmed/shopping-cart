55f613c482a2e8058994f16c9f1351ad3bfd95fd
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-05 22:30:21 +0530

    signup api

--- src/api/signUpAPI.js
+++ src/api/signUpAPI.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-export default (authToken) =>
-  axios.get('http://localhost:3000/users/me', {
-    headers: {
-      'Content-Type': 'application/json',
-      'x-auth': authToken
-    }
-  }).then(response => response.data)
+export default (user) =>
+  axios.post('http://localhost:3000/users',
+    {...user},
+    {headers: {'Content-Type': 'application/json'}})
+    .then((response) => response)
     .catch(err => {
       throw err;
     });
