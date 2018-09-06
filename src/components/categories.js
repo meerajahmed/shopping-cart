7d6f16dedc524bbedcfaeb1daf156de29ff68999
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-06 22:32:01 +0530

    category component

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,14 +1,52 @@
 import React from 'react';
-import Book from "./book";
+import Category from "./category";
 
-const Books = ({books}) => {
-  const bookMap = books.map((book) =>
-    (<Book key={book._id} book={book}/>));
+/*const categoryMapIterator = function () {
+ let properties = Object.keys(this);
+ let count = 0;
+ let isDone = false;
+ let next = () => {
+ if(count >= properties.length){
+ isDone = true;
+ }
+ return { done: isDone, value: this[properties[count++]] }
+ };
+ return {next};
+ };*/
+
+//categoryMap[Symbol.iterator] = categoryMapIterator;
+
+
+const Categories = ({books, searchCategory, handleCategoryChange}) => {
+
+  const categoryMap = books.reduce((categoryMap, {category}) => {
+    if (category in categoryMap) {
+      categoryMap[category]++;
+    } else {
+      categoryMap[category] = 1;
+    }
+    return categoryMap;
+  }, {});
+
+  const categoryList = Object.keys(categoryMap);
+
   return (
     <div className="row">
-      {bookMap}
+      <div className="col-lg-12">
+        <ul className="list-group text-primary list-group-flush shadow mb-4">
+          {categoryList
+            .map((category) =>
+              ((<Category
+                key={category}
+                category={category}
+                isActive={(searchCategory === category)}
+                count={categoryMap[category]}
+                handleCategoryChange={handleCategoryChange}
+              />)))}
+        </ul>
+      </div>
     </div>
   );
 };
 
-export default Books;
+export default Categories;
--- src/components/category.js
+++ src/components/category.js
@@ -1,3 +1,20 @@
-/**
- * Created by meeahmed on 9/3/2018.
- */
+import React from 'react';
+
+const getActiveListClass = ({isActive}) => isActive ? 'list-group-item-action active' : '';
+
+const getActiveBadgeClass = ({isActive}) => isActive ? 'badge-light text-primary' : 'badge-primary';
+
+const Category = (props) => {
+  return (
+    <li
+      className={`list-group-item d-flex justify-content-between align-items-center ${getActiveListClass(props)}`}
+      onClick={() => props.handleCategoryChange(props.category)}
+    >
+      {props.category}
+      <span className={`badge badge-pill ${getActiveBadgeClass(props)}`}>{props.count}</span>
+    </li>
+    
+  )
+};
+
+export default Category;
