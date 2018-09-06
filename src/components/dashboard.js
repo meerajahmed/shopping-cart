59344dd8e7d9d46ffd0c351894ba3781be67f1d2
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-06 22:32:31 +0530

    dashboard component

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,87 +1,35 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
+import Books from "./books";
+import SearchBar from "./search-bar";
+import Categories from "./categories";
 
-//import { userActions } from '../_actions';
+export default class Dashboard extends Component {
 
-export default class SignUp extends Component {
-  state = {
-    user: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: ''
-    },
-    submitted: false
-  };
-
-  handleChange = (event) => {
-    const {name, value} = event.target;
-    const {user} = this.state;
-    this.setState({
-      user: {
-        ...user,
-        [name]: value
-      }
-    });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.setState({submitted: true});
-    const {user} = this.state;
-    if (user.firstName && user.lastName && user.email && user.password) {
-      this.props.handleSubmit(user);
-    }
-  };
+  componentDidMount() {
+    this.props.getBooks();
+  }
 
   render() {
-    //const {registering} = this.props;
-    const {user, submitted} = this.state;
     return (
-      <div className="row justify-content-center mt-5">
-        <div className="col-md-6">
-          <h2>Sign Up</h2>
-          <form name="form" onSubmit={this.handleSubmit}>
-            <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
-              <label htmlFor="firstName">First Name</label>
-              <input type="text" className="form-control" name="firstName" value={user.firstName}
-                     onChange={this.handleChange}/>
-              {submitted && !user.firstName &&
-              <div className="help-block">First Name is required</div>
-              }
-            </div>
-            <div className={'form-group' + (submitted && !user.lastName ? ' has-error' : '')}>
-              <label htmlFor="lastName">Last Name</label>
-              <input type="text" className="form-control" name="lastName" value={user.lastName}
-                     onChange={this.handleChange}/>
-              {submitted && !user.lastName &&
-              <div className="help-block">Last Name is required</div>
-              }
-            </div>
-            <div className={'form-group' + (submitted && !user.email ? ' has-error' : '')}>
-              <label htmlFor="email">Email</label>
-              <input type="text" className="form-control" name="email" value={user.email}
-                     onChange={this.handleChange}/>
-              {submitted && !user.email &&
-              <div className="help-block">Email is required</div>
-              }
-            </div>
-            <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
-              <label htmlFor="password">Password</label>
-              <input type="password" className="form-control" name="password" value={user.password}
-                     onChange={this.handleChange}/>
-              {submitted && !user.password &&
-              <div className="help-block">Password is required</div>
-              }
-            </div>
-            <div className="form-group">
-              <button className="btn btn-primary">Sign Up</button>
-              <Link to="/" className="btn btn-link">Cancel</Link>
-            </div>
-          </form>
+        <div className="row my-5">
+          <div className="col-lg-4">
+            <SearchBar
+              searchText={this.props.searchText}
+              handleSearchInputChange={this.props.handleSearchInputChange} />
+            <Categories
+              books={this.props.books}
+              searchCategory={this.props.searchCategory}
+              handleCategoryChange={this.props.handleCategoryChange}
+            />
+          </div>
+          <div className="col-lg-8">
+            <Books
+              books={this.props.visibleBooks}
+              searchText={this.props.searchText}
+              handleAddToCart={this.props.handleAddToCart}
+            />
+          </div>
         </div>
-      </div>
     );
   }
 }
