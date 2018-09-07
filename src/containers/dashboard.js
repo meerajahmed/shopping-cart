64b9eea98bc36dcf2c7078b715c0280e98f3f8bf
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-07 22:35:18 +0530

    dashboard container

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -1,12 +1,29 @@
 import {connect} from "react-redux";
-import Header from "../components/header";
-import {logout} from "../actions/auth";
-import {getUserName} from "../selectors/user";
+import Dashboard from "../components/dashboard";
+import {selectBooks, selectVisibleBooks} from "../selectors/books";
+import {getBooks} from "../actions/books";
+import {categoryChange, searchInputChange, setSearchCategory} from "../actions/search";
+import {getSearchCategory, getSearchText} from "../selectors/search";
+import {addToCart} from "../actions/cart";
 
-const mapStateToProps = (state) => ({userName: getUserName(state)});
+const mapStateToProps = (state) => {
+  const searchText = getSearchText(state);
+  const searchCategory = getSearchCategory(state);
+  const books = selectBooks(state, searchText);
+  const visibleBooks = selectVisibleBooks(state, searchText, searchCategory);
+  return {
+    searchText,
+    searchCategory,
+    books,
+    visibleBooks
+  }
+};
 
 const mapDispatchToProps = (dispatch) => ({
-  startLogout: () => dispatch(logout())
+  getBooks: () => dispatch(getBooks()),
+  handleSearchInputChange: (searchText) => dispatch(searchInputChange(searchText)),
+  handleAddToCart: (book) => dispatch(addToCart(book)),
+  handleCategoryChange: (category) => dispatch(setSearchCategory(category))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
