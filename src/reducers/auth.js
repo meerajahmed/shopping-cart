a85862bbc4c21e0bc44bc54a059b69a15cf421d6
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-08 22:36:33 +0530

    auth reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,10 +1,12 @@
-export default (state = {}, action) => {
-  switch (action.type) {
-    case 'LOGIN':
-      return {
-        uid: action.uid
-      };
-    case 'LOGOUT':
+import {REMOVE_AUTH_TOKEN, SET_AUTH_TOKEN} from "../actions/auth";
+
+export default (state = {}, {type, payload}) => {
+  switch (type) {
+    case SET_AUTH_TOKEN:
+      const {token} = payload;
+      return {token};
+
+    case REMOVE_AUTH_TOKEN:
       return {};
     default:
       return state;
