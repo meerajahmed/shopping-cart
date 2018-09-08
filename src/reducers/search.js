0a4b7b52d55df603faeae04b2bde055f490e9099
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-08 22:37:35 +0530

    search reducer

--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,16 +1,27 @@
+import {SET_SEARCH_CATEGORY, SET_SEARCH_TEXT} from "../actions/search";
 const defaultState = {
   searchText: '',
-  category: ''
+  searchCategory: ''
 };
 
 export default (state = defaultState, {type, payload}) => {
   switch (type) {
-    case 'SET_SEARCH_TEXT':
+    case SET_SEARCH_TEXT:
       const {searchText} = payload;
       return {
         ...state,
         searchText
       };
+
+    case SET_SEARCH_CATEGORY:
+      let {searchCategory} = payload;
+      searchCategory = (searchCategory !== state.searchCategory ) ?
+        searchCategory : '';
+      return {
+        ...state,
+        searchCategory
+      };
+
     default:
       return state;
   }
