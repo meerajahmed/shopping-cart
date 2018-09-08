92a96e2e9f40f48adc5098a869c626bbd7b3505a
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-08 22:37:48 +0530

    user reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,3 +1,16 @@
-/**
- * Created by meeahmed on 9/2/2018.
- */
+import {SET_USER_INFO, REMOVE_USER_INFO} from "../actions/user";
+
+export default (state = {}, {type, payload = {}}) => {
+  switch (type) {
+    case SET_USER_INFO:
+      const {firstName, email} = payload;
+      return {
+        firstName,
+        email
+      };
+    case REMOVE_USER_INFO:
+      return {};
+    default:
+      return state;
+  }
+};
