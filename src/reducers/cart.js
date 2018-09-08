1ff1e88480dfacadedc3eaeb01f97ff85c7d0e6f
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-08 22:37:05 +0530

    cart reducer

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,13 +1,28 @@
 import {REMOVE_AUTH_TOKEN, SET_AUTH_TOKEN} from "../actions/auth";
+import {ADD_CART_ITEM, ADD_CART_ITEMS, REMOVE_CART_ITEM, REMOVE_CART_ITEMS, UPDATE_CART} from "../actions/cart";
 
-export default (state = {}, {type, payload}) => {
+export default (state = [], {type, payload}) => {
   switch (type) {
-    case SET_AUTH_TOKEN:
-      const {token} = payload;
-      return {token};
+    case ADD_CART_ITEM:
+      return [...state, {...payload.cartItem}];
 
-    case REMOVE_AUTH_TOKEN:
-      return {};
+    case ADD_CART_ITEMS:
+      return [...payload.cartItems];
+
+    case REMOVE_CART_ITEM:
+      return state.filter((cartItem) => (cartItem._id !== payload.cartId));
+
+    case REMOVE_CART_ITEMS:
+      return [];
+
+    case UPDATE_CART:
+      return state.map((cartItem) => {
+        if (cartItem._id === payload.cartId) {
+          cartItem.quantity = payload.quantity;
+        }
+        return cartItem;
+      });
+
     default:
       return state;
   }
