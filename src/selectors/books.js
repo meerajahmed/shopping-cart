56f92e40558e379614f9e1c8b6533b502d7c0997
Meeraj Ahmed <meerajahmed@gmail.com>
2018-09-10 17:36:38 +0530

    books selectors

--- src/selectors/books.js
+++ src/selectors/books.js
@@ -1,3 +1,20 @@
-/**
- * Created by meeahmed on 9/2/2018.
- */
+export const selectAllBooks = ({books}) => books;
+
+// search books without category
+export const selectBooks = ({books}, searchText) => {
+  const text = searchText.toLowerCase();
+  return books.filter((book) => (!text || book.title.toLowerCase().indexOf(text) > -1));
+};
+
+// search books with category
+export const selectVisibleBooks = ({books}, searchText, searchCategory) => {
+  const text = searchText.toLowerCase();
+  return books.filter((book) => ((!searchCategory || book.category === searchCategory) &&
+  (!text || book.title.toLowerCase().indexOf(text) > -1)));
+};
+
+
+export const selectBook = ({books}, bookId) => books.find(({_id}) => _id === bookId);
+
+
+
